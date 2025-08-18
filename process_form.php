d16172f152ede448d0e902836189cf4293a0cdd4
Ayush singh <ayushsingh@Ayushs-MacBook-Air-2.local>
2025-08-18 22:25:30 +0530

    Update process_form.php with latest changes

--- process_form.php
+++ process_form.php
@@ -41,8 +41,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'ayushgnida@gmail.com';     // your Gmail
-        $mail->Password   = 'diwlzghmtbysfrkp';         // Gmail App password
+        $mail->Username   = '';     // your Gmail
+        $mail->Password   = '';         // Gmail App password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
